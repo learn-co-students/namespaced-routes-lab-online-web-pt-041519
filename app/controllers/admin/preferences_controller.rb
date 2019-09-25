cdb9f38300434d4fbfc928e12886acea439d8602
aparkening <aaron.parkening@gmail.com>
2019-09-25 16:56:14 -0400

    Done. Add preferences and adjust song and artist views to reflect preference fields.

---
+++ app/controllers/admin/preferences_controller.rb
@@ -0,0 +1,13 @@
+class Admin::PreferencesController < ApplicationController
+
+  def index
+
+    # Song sort order by name
+    # Artist sort order by title
+
+    # Allow add new songs
+    # Allow add new artists
+
+  end
+
+end
