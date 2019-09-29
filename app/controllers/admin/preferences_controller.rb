23e16873f4cf0bdae14861fceed036a411d3f12a
github username <github email address>
2019-09-29 19:27:30 -0400

    Done.

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,8 +1,6 @@
 class Admin::PreferencesController < ApplicationController
-  prepend_view_path 'app/views/admin'
 
   def index
     @preferences = Preference.first
-    render template: 'admin/index'
   end
 end
