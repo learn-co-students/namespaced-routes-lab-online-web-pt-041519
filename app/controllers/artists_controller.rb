74d6ae6d50544813dc2c2142435a012ffb4bed9b
sullycodes <dansullivan19@gmail.com>
2019-09-30 22:18:27 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -9,6 +9,11 @@ class ArtistsController < ApplicationController
 
   def new
     @artist = Artist.new
+    p = Preference.first
+    if p.allow_create_artists == false
+      redirect_to artists_path
+    end
+
   end
 
   def create
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,6 +26,10 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    p = Preference.first
+    if p.allow_create_songs == false
+      redirect_to songs_path
+    end
   end
 
   def create
